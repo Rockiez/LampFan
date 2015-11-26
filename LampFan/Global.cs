67cf44f11666b14c35cb1f8f52a3d3499669f1e8
rockiez <simpi@foxmail.com>
2015-11-26 15:01:34 +0800

    up

---
+++ LampFan/Global.cs
@@ -0,0 +1,48 @@
+﻿using ICS.Acquisition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICS.LampAndFan
+{
+    public class Global
+    {
+
+        public static ZigBee ZigBeeProvider
+        {
+            get
+            {
+                return (ZigBee)ClassFactory.GetProvider(equipmentCategory.ZigBee, Global.ComSetting);
+            }
+        }
+
+        public static ADAM4150 ADAM4150Provider
+        {
+            get
+            {
+                return (ADAM4150)ClassFactory.GetProvider(equipmentCategory.ADAM4150, Global.ComSetting);
+            }
+        }
+
+        public static ICS.Models.Com.ComSettingModel _ComSetting = null;
+
+        public static ICS.Models.Com.ComSettingModel ComSetting
+        {
+            get
+            {
+                if (_ComSetting == null)
+                {
+                    _ComSetting = new ICS.Common.SettingHelper<ICS.Models.Com.ComSettingModel>().GetSetting();
+                    _ComSetting.ZigbeeCom = "Com3";
+                    _ComSetting.AnalogQuantityCom = "Com2";
+                    _ComSetting.DigitalQuantityCom = "Com2";
+              
+                }
+
+                return _ComSetting;
+            }
+        }
+    }
+}
---
+++ LampFan/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ICS.LampAndFan;
+using ICS.Acquisition;
+using ICS.Common;
+using ICS.Models;
+
+namespace LampFan
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    /// 
+
+    public class onofflamp
+    {
+        public bool lampstate = false;
+        public ADAM4150FuncID oncode, offcode;
+        public Image lampimg;
+        public Image buttonimg;
+        public string errormess;
+
+        public void onoffmain()
+        {
+            var state = Global.ADAM4150Provider.CheckSerialPort(Global.ADAM4150Provider.ADAM4017Provider);
+            if (state.Status == RunStatus.Failure)
+            {
+                MessageBox.Show(state.ResultMessage);
+            }
+            else
+            {
+                if (Global.ADAM4150Provider.OnOff(lampstate ? offcode : oncode))
+                {
+                    lampstate = !lampstate;
+                    resetimg();
+                }
+                else
+                {
+                    MessageBox.Show(errormess);
+                }
+            }
+        }
+
+
+        void resetimg()
+        {
+            if (lampstate)
+            {
+                lampimg.Source = new BitmapImage(new Uri("Resources/lamp_on", UriKind.Relative));
+                buttonimg.Source = new BitmapImage(new Uri("Resources/btn_switch_on", UriKind.Relative)); 
+            }
+            else
+            {
+                lampimg.Source = new BitmapImage(new Uri("Resources/lamp_off", UriKind.Relative));
+                buttonimg.Source = new BitmapImage(new Uri("Resources/btn_switch_off", UriKind.Relative)); 
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        onofflamp controlstreet;
+        onofflamp controlcorride;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controlcorride = new onofflamp { oncode = ADAM4150FuncID.OnCorridorLamp, offcode = ADAM4150FuncID.OffCorridorLamp, lampimg = imgc, buttonimg = imgcc, errormess = "操作楼道灯失败" };
+            controlstreet = new onofflamp { oncode = ADAM4150FuncID.OnStreetLamp, offcode = ADAM4150FuncID.OffStreetLamp, lampimg = imgs, buttonimg = imgsc, errormess = "操作街道灯失败" };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+         WindowState = WindowState.Minimized;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+        Close();
+        }
+
+        ResultEntity state = Global.ZigBeeProvider.CheckSerialPort(Global.ZigBeeProvider);
+
+        void fancontrol(string errormess, ZigBeeFuncID id)
+        {
+            if (state.Status == RunStatus.Failure)
+            {
+                MessageBox.Show(state.ResultMessage);
+            }
+            else
+            {
+                if (! Global.ZigBeeProvider.GetSet(id))
+                {
+                    MessageBox.Show(errormess);
+                }
+
+            }
+        }
+        private void onbutton1(object sender, RoutedEventArgs e)
+        {
+            fancontrol("操作风扇1失败", ZigBeeFuncID.OnFan1);
+        }
+
+        private void offbutton1(object sender, RoutedEventArgs e)
+        {
+            fancontrol("操作风扇1失败", ZigBeeFuncID.OffFan1);
+        }
+
+        private void onbutton2(object sender, RoutedEventArgs e)
+        {
+            fancontrol("操作风扇2失败", ZigBeeFuncID.OnFan2);
+        }
+
+        private void offbutton2(object sender, RoutedEventArgs e)
+        {
+            fancontrol("操作风扇2失败", ZigBeeFuncID.OffFan2);
+        }
+        private void Imgs_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        controlstreet.onoffmain();
+        }
+
+        private void Imgsc_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        controlstreet.onoffmain();
+        }
+
+        private void Imgc_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        controlcorride.onoffmain();
+        }
+
+        private void Imgcc_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        controlcorride.onoffmain();
+        }
+    }
+}
